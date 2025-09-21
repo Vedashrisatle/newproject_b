468d7b27a3a8fad9cebb8f455b0d12ddcdcd3082
vedashri <vedashrisatle@gmail.com>
2025-09-21 13:25:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,15 @@ const { VertexAI } = require('@google-cloud/vertexai');
 const app = express();
 
 // CORS setup - allow your frontend
-app.use(
-  cors({
-    origin: "https://new-project-three-flax.vercel.app", // **no trailing slash**
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
 app.use(express.json());
 
-const PROJECT_ID ="genai-471818";
+const PROJECT_ID = process.env.PROJECT_ID;
 const LOCATION = 'us';
-const PROCESSOR_ID = "54be1a4c93565429";
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_JSON);
+const PROCESSOR_ID = process.env.PROCESSOR_ID;
+
 // Google Document AI Auth
 const client = new google.auth.GoogleAuth({
-  credentials: credentials: {
+  credentials: {
     client_email: process.env.client_email,
     private_key: process.env.private_key.replace(/\\n/g, "\n"),
     private_key_id: process.env.private_key_id,
@@ -44,12 +36,12 @@ const documentai = google.documentai({
 // Multer for file upload
 const upload = multer({ storage: multer.memoryStorage() });
 
+
 // Vertex AI setup
 const vertexAI = new VertexAI({
   project: PROJECT_ID,
   location: 'us-central1',
 });
-
 
 const model = vertexAI.getGenerativeModel({
   model: 'gemini-2.5-flash-lite',
@@ -114,6 +106,7 @@ const encodedFile = fileData.toString("base64");
 
 // 👇 This is the correct way for Vercel (don’t use app.listen)
 module.exports = app;
+
 
 
 
